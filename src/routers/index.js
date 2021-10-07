438369e5ee7c40c3ea31fb54dfae77300d82c7f4
renatomak <renato.mark.silva@gmail.com>
2021-10-07 09:42:31 -0300

    add router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { addUser } = require('../controllers');
 
 const router = express.Router();
+
+router.post('/users', addUser);
 
 module.exports = router;
