85f08d7600dc9f64ef3835e4bc2e81cf4f395412
renatomak <renato.mark.silva@gmail.com>
2021-10-07 14:32:48 -0300

    test to validate the size of the name field

--- tests/createUser.test.js
+++ tests/createUser.test.js
@@ -38,4 +38,18 @@ describe('1 - endpoint POST /users', () => {
         expect(json).toEqual({ message: 'O campo "name" é obrigatório' });
       });
   });
+
+  test('It will be validated that it is not possible to register a user with a name of less than 3 characters', async () => {
+    await frisby
+      .post(`${url}/users`, {
+        name: 'us',
+        email: 'user1@test.com.br',
+        password: '123456',
+      })
+      .expect('status', 400)
+      .then((responseCreate) => {
+        const { json } = responseCreate;
+        expect(json).toEqual({ message: 'O campo "name" deve ter pelo menos 3 caracteres' });
+      });
+  });
 });
